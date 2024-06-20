3d5cb10da5ea61996f82cf0a49bfb85f79826b7e
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-20 21:16:58 +0900

    app/(tabs)/index.tsxを編集する

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,15 +3,15 @@ import { StyleSheet, Image } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import tweets from '@/assets/data/tweets';
 
-const tweet = tweets[1];
+const tweet = tweets[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Image src={tweet.user.image} style={styles.userImage} />
       <View style={styles.mainContainer}>
-        <Text>{tweet.user.name}</Text>
-        <Text>{tweet.content}</Text>
+        <Text style={styles.name}>{tweet.user.name}</Text>
+        <Text style={styles.content}>{tweet.content}</Text>
       </View>
     </View>
   );
@@ -29,5 +29,13 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 50,
   },
-  mainContainer: {},
+  mainContainer: {
+    marginLeft: 10,
+  },
+  name: {
+    fontWeight: '600',
+  },
+  content: {
+    lineHeight: 20,
+  },
 });
