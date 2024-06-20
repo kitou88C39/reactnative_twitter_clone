e2ec022cae710d774697c8a58caefeb94b273d07
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-20 20:48:17 +0900

    app/(tabs)/index.tsxを編集する

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import tweets from '@/assets/data/tweets';
+
+const tweet = tweets[0];
 
 export default function TabOneScreen() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
