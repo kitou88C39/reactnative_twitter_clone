bb3762b8b08ce0921cd0b643929dede6502185c3
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-21 09:07:28 +0900

    componentsにTweets.tsxを作成する

---
+++ components/Tweets.tsx
@@ -0,0 +1,11 @@
+const Tweet = () => {
+  return (
+    <View style={styles.container}>
+      <Image src={tweet.user.image} style={styles.userImage} />
+      <View style={styles.mainContainer}>
+        <Text style={styles.name}>{tweet.user.name}</Text>
+        <Text style={styles.content}>{tweet.content}</Text>
+      </View>
+    </View>
+  );
+};
