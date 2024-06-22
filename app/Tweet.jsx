dd85a14f0c46645531ecaf382610da1e206e5a21
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-22 11:12:15 +0900

    appにTweet.jsxを作成する

--- app/Tweet.jsx
+++ app/Tweet.jsx
@@ -1,5 +1,6 @@
 import Tweet from '@/components/Tweet';
+import tweets from '@/assets/data/tweets';
 
-export default Tweet = () => {
-  return <Tweet />;
-};
+export default function TweetScreen() {
+  return <Tweet tweet={tweets[0]} />;
+}
