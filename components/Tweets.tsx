b5538dec7d45e9e779fdc158c3fa8b9c823f2e58
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-22 10:17:38 +0900

    components/Tweets.tsxを作成する

--- components/Tweets.tsx
+++ components/Tweets.tsx
@@ -2,22 +2,6 @@ import { Text, View, Image, StyleSheet } from 'react-native';
 import { TweetType } from '@/type';
 import { Entypo, EvilIcons } from '@expo/vector-icons';
 
-type IconButtonProps = {
-  icon: React.ComponentProps<typeof EvilIcons>['name'];
-  text?: string | number;
-};
-
-const IconButton = ({ icon, text }: IconButtonProps) => {
-  return (
-    <View style={styles.footer}>
-      <View>
-        <EvilIcons name={icon} size={220} color='gray' />
-        <Text style={{ fontSize: 12, color: 'gray' }}>{text}</Text>
-      </View>
-    </View>
-  );
-};
-
 type TweetProps = {
   tweet: TweetType;
 };
