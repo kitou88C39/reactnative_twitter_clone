c6c4167d94a1dad29607c1f1da4966d88cc02885
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-24 20:26:18 +0900

    app/(tabs)/index.tsxを編集する

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, View, FlatList, Pressable } from 'react-native';
 import Tweet from '@/components/Tweet';
 import tweets from '@/assets/data/tweets';
 import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
   return (
@@ -10,9 +11,15 @@ export default function TabOneScreen() {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
       />
-      <Pressable style={styles.floatingButton}>
-        <Entypo name='plus' size={24} color='white' />
-      </Pressable>
+
+      <Link href='/new-tweet' asChild>
+        <Entypo
+          name='plus'
+          size={24}
+          color='white'
+          style={styles.floatingButton}
+        />
+      </Link>
     </View>
   );
 }
