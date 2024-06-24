b92a15d64e2d95ec41d972c99ef6d73ce3673baf
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-24 19:53:54 +0900

    app/new-tweet.tsxを編集する

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,7 +1,11 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function NewTweet() {
-  return <View></View>;
+  return (
+    <View>
+      <Text>Create new tweet</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
