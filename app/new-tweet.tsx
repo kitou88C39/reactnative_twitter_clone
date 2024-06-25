c02a7e34beb21843ed12b419001fee9034bfcb00
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-25 10:01:20 +0900

    app/new-tweet.tsxを編集する

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
 const user = {
   id: 'u1',
@@ -11,10 +11,41 @@ const user = {
 export default function NewTweet() {
   return (
     <View>
-      <Image />
+      <Image src={user.image} style={styles.image} />
       <Text>Create new tweet</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#1C9BF0',
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+  },
+  image: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+});
